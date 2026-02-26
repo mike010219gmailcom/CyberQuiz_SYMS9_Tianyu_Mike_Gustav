5b6fa7e43311ef1446ccff8c049671ce26a45b83
Gustav Lenander <gustav.lenander@icloud.com>
2026-02-26 11:01:37 +0100

    Added models

---
+++ CyberQuiz_DAL/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CyberQuiz.DAL.Models;
+
+public class Question
+{
+    public int Id { get; set; }
+
+    [Required, MaxLength(800)]
+    public required string Text { get; set; }
+
+    [ForeignKey(nameof(SubCategory))]
+    public int SubCategoryId { get; set; }
+    public SubCategory? SubCategory { get; set; }
+
+    public ICollection<AnswerOption> AnswerOptions { get; set; } = new List<AnswerOption>();
+}
---
+++ CyberQuiz_DAL/Models/UserResult.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CyberQuiz.DAL.Models;
+
+public class UserResult
+{
+    public int Id { get; set; }
+
+    
+    [Required, MaxLength(450)]
+    public required string UserId { get; set; }
+
+    [ForeignKey(nameof(Question))]
+    public int QuestionId { get; set; }
+    public Question? Question { get; set; }
+
+  
+    [ForeignKey(nameof(SelectedAnswerOption))]
+    public int SelectedAnswerOptionId { get; set; }
+    public AnswerOption? SelectedAnswerOption { get; set; }
+
+    public bool IsCorrect { get; set; }
+
+    public DateTime AnsweredAtUtc { get; set; } = DateTime.UtcNow;
+
+    public int SubCategoryId { get; set; }
+}
