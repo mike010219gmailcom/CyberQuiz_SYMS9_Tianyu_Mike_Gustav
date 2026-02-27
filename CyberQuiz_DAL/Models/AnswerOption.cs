12c640cd2c2f05fa7ea0277ada211f9783173876
Gustav Lenander <gustav.lenander@icloud.com>
2026-02-27 14:38:34 +0100

    Added migrations

---
+++ CyberQuiz_DAL/Models/AnswerOption.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CyberQuiz.DAL.Models;
+
+public class AnswerOption
+{
+    public int Id { get; set; }
+
+    [Required, MaxLength(400)]
+    public required string Text { get; set; }
+
+    public bool IsCorrect { get; set; }
+
+    [ForeignKey(nameof(Question))]
+    public int QuestionId { get; set; }
+    public Question? Question { get; set; }
+    public int Order { get; set; }
+}
