a116df20ac1f3706e893dbff379003fdf230c515
Tianyu Shi <tianyushi97@gmail.com>
2026-03-02 17:48:36 +0100

    skapa interface iCateService iSubCateService & iQuizService

---
+++ CyberQuiz_BLL/Services/ISubCategoryService.cs
@@ -0,0 +1,13 @@
+﻿using CyberQuiz_BLL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuiz_BLL.Services
+{
+    public interface ISubCategoryService
+    {
+        // Get/Read
+        Task<List<SubCategoryDto>> GetSubCategoryAsync(int categoryId);
+    }
+}
--- CyberQuiz_Shared/DTOs/CategoryDTO.cs
+++ CyberQuiz_Shared/DTOs/CategoryDTO.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace CyberQuiz_BLL.DTOs
 {
-    public class CategoryDTO
+    public class CategoryDto
     {
         public int ID { get; set; }
         public string Name { get; set; }
