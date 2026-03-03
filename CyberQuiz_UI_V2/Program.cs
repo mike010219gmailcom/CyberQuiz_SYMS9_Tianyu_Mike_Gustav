d61bb042aad0c20b0a2ffb1d84788c12d71bafbb
mike_loku <mike010219@gmail.com>
2026-03-03 18:09:57 +0100

    Register page

--- CyberQuiz_UI_V2/Program.cs
+++ CyberQuiz_UI_V2/Program.cs
@@ -6,6 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Register HttpClient so components can inject HttpClient for API calls
+builder.Services.AddHttpClient();
+builder.Services.AddScoped(sp => sp.GetRequiredService<System.Net.Http.IHttpClientFactory>().CreateClient());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
