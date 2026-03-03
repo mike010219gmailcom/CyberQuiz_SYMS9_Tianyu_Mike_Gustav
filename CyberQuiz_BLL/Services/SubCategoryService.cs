bc4b576f52155e6af3146eb4d64d679f5ba05bb6
Tianyu Shi <tianyushi97@gmail.com>
2026-03-03 09:34:05 +0100

    Implement CategoryService

---
+++ CyberQuiz_BLL/Services/SubCategoryService.cs
@@ -0,0 +1,25 @@
+﻿using CyberQuiz_BLL.DTOs;
+using CyberQuiz_DAL.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuiz_BLL.Services
+{
+    public class SubCategoryService
+    {
+        private readonly CyberQuizDbContext _context;
+        public SubCategoryService(CyberQuizDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SubCategoryDto>> GetAllSubCategoryAsync()
+        {
+            // DbContext only includes Categories
+            // Show subCategory with condition CategoryId
+
+        }
+    }
+}
