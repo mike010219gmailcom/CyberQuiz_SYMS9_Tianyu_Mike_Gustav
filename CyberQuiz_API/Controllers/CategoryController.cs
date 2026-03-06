36a5e3084a3ba47727f3e1eba3ec20047621f85e
Tianyu Shi <tianyushi97@gmail.com>
2026-03-06 09:39:07 +0100

    add repo in program.cs

---
+++ CyberQuiz_API/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿
+using CyberQuiz_BLL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+
+namespace CyberQuiz_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategoriesAsync(CancellationToken ct)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrWhiteSpace(userId))
+                return Unauthorized(new { message = "Not logged in" });
+
+            var categories = await _categoryService.GetAllCategoriesAsync(userId, ct);
+
+            return Ok(categories);
+        }
+    }
+}
